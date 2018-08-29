ac364cafed34e1a19723262fd7a3409b96a787e6
justinandjesus <justinandjesus@gmail.com>
2018-08-29 16:54:19 +0800

    new icons and toast for app update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Todos from './components/Todos/Todos';
 import Aux from './Hoc/Aux/Aux';
-
+import Toast from './components/Toast/Toast';
 
 class App extends React.Component {
+  state = {
+    updateAvailable: false
+  }
   componentDidMount(){
     console.log("[App] Component Did Mount");
+    window['isUpdateAvailable']
+    	.then(isAvailable => {
+    		if (isAvailable) {
+    		  this.setState(prevState => ({updateAvailable:true}));
+    		}
+    	});
   }
 
   render() {
@@ -14,6 +23,7 @@ class App extends React.Component {
         <Aux>
           <Header />
           <Todos />
+          {this.state.updateAvailable && <Toast />}
         </Aux>
     );
   }
